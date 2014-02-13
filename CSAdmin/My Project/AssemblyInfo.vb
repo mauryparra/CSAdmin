ec98600176155e2140af166c997e9335e0922923
Mauricio Parra Casado <mauryparra@gmail.com>
2014-02-13 09:14:54 -0300

    Nuevo Proyecto
    Nuevo proyecto Visual Basic 2008

---
+++ CSAdmin/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CSAdmin")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Crecer Sanos")> 
+<Assembly: AssemblyProduct("CSAdmin")> 
+<Assembly: AssemblyCopyright("Copyright Crecer Sanos ©  2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("862eb249-3af5-424f-a58e-2478d3c39105")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("es-AR")> 
