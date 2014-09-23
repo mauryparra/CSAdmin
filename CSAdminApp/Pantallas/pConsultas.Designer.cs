f7195353b351ef873a1d57df67f8835f7d0124f4
Mauricio Parra Casado <mauryparra@gmail.com>
2014-09-23 12:16:26 -0300

    Primer Reporte
    Reporte de prueba con asistencias

--- CSAdminApp/Pantallas/pConsultas.Designer.cs
+++ CSAdminApp/Pantallas/pConsultas.Designer.cs
@@ -28,18 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.cSAdminBDEntitiesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.cSAdminBDEntitiesBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // reportViewer
+            // 
+            this.reportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSetAsistencia";
+            reportDataSource1.Value = this.cSAdminBDEntitiesBindingSource;
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer.LocalReport.ReportEmbeddedResource = "CSAdminApp.Reportes.ReporteAsistencia.rdlc";
+            this.reportViewer.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer.Name = "reportViewer";
+            this.reportViewer.Size = new System.Drawing.Size(689, 446);
+            this.reportViewer.TabIndex = 0;
+            // 
+            // cSAdminBDEntitiesBindingSource
+            // 
+            this.cSAdminBDEntitiesBindingSource.DataSource = typeof(CSAdminApp.Clases.CSAdminBDEntities);
             // 
             // pConsultas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.reportViewer);
             this.Name = "pConsultas";
             this.Size = new System.Drawing.Size(689, 446);
+            this.Load += new System.EventHandler(this.pConsultas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cSAdminBDEntitiesBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
+        private System.Windows.Forms.BindingSource cSAdminBDEntitiesBindingSource;
     }
 }
--- CSAdminApp/Pantallas/pConsultas.cs
+++ CSAdminApp/Pantallas/pConsultas.cs
@@ -6,6 +6,8 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using Microsoft.Reporting.WinForms;
 
 namespace CSAdminApp.Pantallas
 {
@@ -14,6 +16,27 @@ namespace CSAdminApp.Pantallas
         public pConsultas()
         {
             InitializeComponent();
+        }
+
+        private void pConsultas_Load(object sender, EventArgs e)
+        {
+            List<Clases.Inasistencias> lstInasistencias = new List<Clases.Inasistencias>();
+
+            lstInasistencias = Main.BDContext.Inasistencias.ToList();
+
+            string exeFolder = Path.GetDirectoryName(Application.StartupPath);
+            string reportPath = Path.Combine(exeFolder, @"Release\Reportes\ReporteAsistencia.rdlc");
+
+            reportViewer.ProcessingMode = ProcessingMode.Local;
+            reportViewer.LocalReport.ReportPath = reportPath;
+
+            ReportDataSource rDataSource = new ReportDataSource();
+            rDataSource.Name = "DataSetAsistencia";
+            rDataSource.Value = lstInasistencias;
+
+            reportViewer.LocalReport.DataSources.Clear();
+            reportViewer.LocalReport.DataSources.Add(rDataSource);
+            reportViewer.RefreshReport();
         }
     }
 }
